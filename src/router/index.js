00c2895aea26a9cbda64f9d9c90d940b7387055a
lingkai5wu <lingkai5wu@outlook.com>
2024-01-05 17:22:16 +0800

    feat: 全局加载进度条

--- src/router/index.js
+++ src/router/index.js
@@ -42,9 +42,13 @@ const router = createRouter({
 export default router
 
 router.beforeEach(async (to) => {
+  // 进度条
+  window.$loading.start()
+
   // 已登录时访问登录页，跳转到首页
   const tokenStore = useTokenStore()
   if (to.path === '/login' && tokenStore.value) {
+    window.$loading.finish()
     return '/'
   }
 
@@ -52,6 +56,7 @@ router.beforeEach(async (to) => {
   const menuStore = useMenuStore()
   if (menuStore.menuOptions.length === 0) {
     if (to.path === '/login') {
+      window.$loading.finish()
       return true
     }
     const result = await listCurUserMenu()
@@ -59,9 +64,11 @@ router.beforeEach(async (to) => {
     menuStore.menuOptions = buildMenuTree(result.data)
     // 想访问的路由可能被 remove
     if (!router.hasRoute(to.name)) {
+      window.$loading.finish()
       return '/'
     }
   }
 
+  window.$loading.finish()
   return true
 })
