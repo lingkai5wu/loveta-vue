d85b5605b87f6957b66af64589e0b57b27941c6f
lingkai5wu <lingkai5wu@outlook.com>
2024-02-03 13:05:12 +0800

    feat: 登录页

--- src/components/auth/TheLogin.vue
+++ src/components/auth/TheLogin.vue
@@ -8,6 +8,31 @@ const formData = ref({
   phone: '18888888888',
   password: 'loveta123'
 })
+const formRef = ref()
+const rules = {
+  phone: [
+    {
+      required: true,
+      validator(rule, value) {
+        if (!value) {
+          return new Error('请输入手机号')
+        }
+        if (!/^1\d{10}$/.test(value)) {
+          return new Error('请输入正确的手机号')
+        }
+        return true
+      },
+      trigger: 'blur'
+    }
+  ],
+  password: [
+    {
+      required: true,
+      message: '请输入密码',
+      trigger: 'blur'
+    }
+  ]
+}
 const isLoading = ref(false)
 
 const authStore = useAuthStore()
@@ -31,7 +56,7 @@ function handelLogin() {
 <template>
   <n-h1>欢迎回来</n-h1>
   <n-h3>登录您的帐户</n-h3>
-  <n-form :model="formData" size="large">
+  <n-form ref="formRef" :model="formData" :rules="rules" size="large">
     <n-form-item label="手机号" path="phone">
       <n-input v-model:value="formData.phone" placeholder="中国大陆手机号" />
     </n-form-item>
